3b830860a9b839d19b31e25bf17a41d033ba5e1f
Stanley Calixte <DuCalixte@gmail.com>
2017-01-23 20:00:56 -0500

    done works prior eslint fix

--- app/config/webpack-dev-server.config.js
+++ app/config/webpack-dev-server.config.js
@@ -1,18 +1,25 @@
-
-var WebpackDevServer = require('webpack-dev-server');
 var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
 var webpackConfig = require('./webpack.config.js');
-var path = require('path');
+
 var port = process.env.PORT || '8080';
+var webpackPort = process.env.WEBPACK_PORT || '8090';
 
 new WebpackDevServer(webpack(webpackConfig), {
-	compress: true,
-	stats: {
-    colors: true
- },
-	historyApiFallback: false,
-	headers: {
-		'Access-Control-Allow-Origin': `http://localhost:${port}`,
-		'Access-Control-Allow-Headers': 'X-Requested-With'
-	}
-}).listen(port);
+  publicPath: '/public/app/',
+  inline: true,
+  hot: true,
+  stats: 'errors-only',
+	colors: true,
+  historyApiFallback: true,
+  headers: {
+    'Access-Control-Allow-Origin': `http://localhost:${port}`,
+    'Access-Control-Allow-Headers': 'X-Requested-With'
+  }
+}).listen(webpackPort, 'localhost', function (err) {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log(`webpack dev server listening on localhost:${webpackPort}`);
+});
---
+++ app/config/webpack.production.config.js
@@ -0,0 +1,21 @@
+const Webpack = require('webpack');
+const config = require('./webpack.base.config.js');
+
+config.module.loaders.push(
+  {
+    test: /.jsx?$/,
+    exclude: /node-modules/,
+    loader: 'babel-loader'
+  }
+);
+
+config.plugins.push(
+  new Webpack.NoErrorsPlugin(),
+  new Webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'production')
+    }
+  })
+);
+
+module.exports = config;
